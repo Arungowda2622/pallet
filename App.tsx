2e213b1e7cca869900b67a5bbbc7ff6c9b3b8d28
arungowda2622 <arungowda2622@gmail.com>
2025-10-31 12:54:11 -0700

    Initial commit of React Native CLI project

--- App.tsx
+++ App.tsx
@@ -11,28 +11,27 @@ import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './src/screens/LoginScreen';
+import HomeScreen from './src/screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
 
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Login"
+          screenOptions={{headerShown:false}}
+        >
+           <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
   );
 }
 
---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+
+const LoginScreen = ({ navigation }: any) => {
+  const handleNav = () => {
+    navigation.navigate('Home');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Pressable onPress={handleNav}>
+        <Text style={styles.text}>Login Screen (Tap to go Home)</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+  text: { fontSize: 20, fontWeight: '600' },
+});
